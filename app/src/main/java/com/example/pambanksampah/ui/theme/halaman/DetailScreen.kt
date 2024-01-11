5c95e8b3a4d7b993bb27863f79fcdbdc9cd63eb3
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 21:52:35 +0700

    add fun ItemDetailsBody(by Nadiv)

--- app/src/main/java/com/example/pambanksampah/ui/theme/halaman/DetailScreen.kt
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/DetailScreen.kt
@@ -10,12 +10,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
@@ -24,6 +30,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.pambanksampah.R
 import com.example.pambanksampah.data.Pelapor
+import com.example.pambanksampah.model.ItemDetailsUiState
+import com.example.pambanksampah.model.toPelapor
 import com.example.pambanksampah.navigasi.DestinasiNavigasi
 
 object DetailsDestination : DestinasiNavigasi {
@@ -31,6 +39,46 @@ object DetailsDestination : DestinasiNavigasi {
     override val titleRes = R.string.detail_pelapor
     const val pelaporIdArg = "itemId"
     val routeWithArgs = "$route/{$pelaporIdArg}"
+}
+@Composable
+private fun ItemDetailsBody(
+    itemDetailsUiState: ItemDetailsUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable {
+            mutableStateOf(false)
+        }
+        ItemDetails(
+            pelapor = itemDetailsUiState.detailPelapor.toPelapor(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            colors = ButtonDefaults.buttonColors(Color(0xFF4CAF50)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
 }
 
 @Composable
