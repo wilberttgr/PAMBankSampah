501693bd7bed2e76a9a948ed9dd6ebf6002c3892
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 21:04:49 +0700

    add object DestinasiLogin(by Wilbert)

---
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.example.pambanksampah.ui.theme.halaman
+
+import com.example.pambanksampah.R
+import com.example.pambanksampah.navigasi.DestinasiNavigasi
+
+object DestinasiLogin : DestinasiNavigasi {
+    override val route = "jenis"
+    override val titleRes = R.string.app_name
+}
