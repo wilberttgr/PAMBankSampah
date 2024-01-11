05be8443ca1b1c723fc5ab4683a18b4b72c04817
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 20:35:11 +0700

    add fun AddMedicationScreen(by Wilbert)

--- app/src/main/java/com/example/pambanksampah/ui/theme/halaman/Form.kt
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/Form.kt
@@ -8,27 +8,75 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pambanksampah.R
 import com.example.pambanksampah.model.DetailPelapor
+import com.example.pambanksampah.model.InputViewModel
+import com.example.pambanksampah.model.PenyediaViewModel
 import com.example.pambanksampah.model.UIStatePelapor
 import com.example.pambanksampah.navigasi.DestinasiNavigasi
+import com.example.pambanksampah.navigasi.SampahTopAppBar
+import kotlinx.coroutines.launch
 
 object DestinasiForm: DestinasiNavigasi {
     override val route = "form"
     override val titleRes = R.string.form_pelapor
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddMedicationScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InputViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        topBar = {
+            SampahTopAppBar(
+                title = "Jemput Sampah",
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        modifier = modifier
+    ) { innerpadding ->
+        EntryPelaporBody(
+            uiStatePelapor = viewModel.uiStatePelapor,
+            onPelaporValueChange = viewModel::updateUiState,
+            onBack = navigateBack,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.savePelapor()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerpadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+        )
+    }
 }
 @Composable
 fun EntryPelaporBody(
