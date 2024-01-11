6a81c7f1692977cfb1e75eb69163100cb7b57ab9
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 20:25:59 +0700

    add object DestinasiJenis and DestinasiHistory(by Wilbert)

---
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/HistoryScreen.kt
@@ -0,0 +1,10 @@
+package com.example.pambanksampah.ui.theme.halaman
+
+import com.example.pambanksampah.R
+import com.example.pambanksampah.navigasi.DestinasiNavigasi
+
+// Assuming the history data is stored in a list
+object DestinasiHistory : DestinasiNavigasi {
+    override val route = "history"
+    override val titleRes = R.string.history
+}
--- app/src/main/java/com/example/pambanksampah/ui/theme/halaman/Jenis.kt
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/Jenis.kt
@@ -3,9 +3,7 @@ package com.example.pambanksampah.ui.theme.halaman
 import com.example.pambanksampah.R
 import com.example.pambanksampah.navigasi.DestinasiNavigasi
 
-object DestinasiHome : DestinasiNavigasi {
-    override val route = "home"
-    override val titleRes = R.string.app_name
+object DestinasiJenis : DestinasiNavigasi {
+    override val route = "jenis"
+    override val titleRes = R.string.jenis
 }
-
-
