00b8f10fde4e9f0749125c3caddd843c5ba2cf15
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 21:44:05 +0700

    add ItemDetailsRow (by Nadiv)

--- app/src/main/java/com/example/pambanksampah/ui/theme/halaman/DetailScreen.kt
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/DetailScreen.kt
@@ -1,5 +1,15 @@
 package com.example.pambanksampah.ui.theme.halaman
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.example.pambanksampah.R
 import com.example.pambanksampah.navigasi.DestinasiNavigasi
 
@@ -8,4 +18,22 @@ object DetailsDestination : DestinasiNavigasi {
     override val titleRes = R.string.detail_pelapor
     const val pelaporIdArg = "itemId"
     val routeWithArgs = "$route/{$pelaporIdArg}"
+}
+
+@Composable
+private fun ItemDetailsRow(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(
+            text = stringResource(labelResID),
+            style = MaterialTheme.typography.bodyMedium.copy(color = Color.Black)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = itemDetail,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyMedium.copy(color = Color.Black)
+        )
+    }
 }
