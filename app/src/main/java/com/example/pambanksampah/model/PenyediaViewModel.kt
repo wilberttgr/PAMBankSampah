70ffc5a5d0bfec5fa0c57f6f3e6d7846b0cf7970
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 19:35:42 +0700

    add Fungsi ekstensi query(by Wilbert)

---
+++ app/src/main/java/com/example/pambanksampah/model/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.pambanksampah.model
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pambanksampah.AplikasiBankSampah
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HistoryViewModel(aplikasiSampah().container.repositori) }
+        initializer {
+            InputViewModel(aplikasiSampah().container.repositori)
+        }
+
+    }
+
+    fun CreationExtras.aplikasiSampah(): AplikasiBankSampah =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiBankSampah)
+}
