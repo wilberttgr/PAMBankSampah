dab1c113297b5f5dbfba405669462d4fab865114
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 20:32:14 +0700

    add fun SampahTopAppBar(by Wilbert)

---
+++ app/src/main/java/com/example/pambanksampah/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,50 @@
+package com.example.pambanksampah.navigasi
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SampahTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(
+                    onClick = navigateUp,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .size(40.dp)
+                        .background(
+                            color = Color(0xFF4CAF50),
+                            shape = CircleShape
+                        )) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack, contentDescription = ""
+                    )
+                }
+            }
+        }
+    )
+}
