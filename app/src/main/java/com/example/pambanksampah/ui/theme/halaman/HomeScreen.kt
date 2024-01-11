39b199e18c04ea1ab4ee8f1148846335fc659a83
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 20:30:16 +0700

    add fun BankSampahApp(by Wilbert)

--- app/src/main/java/com/example/pambanksampah/ui/theme/halaman/HomeScreen.kt
+++ app/src/main/java/com/example/pambanksampah/ui/theme/halaman/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.example.pambanksampah.ui.theme.halaman
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,15 +13,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -31,7 +38,82 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes = R.string.app_name
 }
 
+@Composable
+fun BankSampahApp(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF4CAF50),
+                        Color(0xFFFFFFFF)
+                    )
+                )
+            )
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Card 1
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .shadow(16.dp, RoundedCornerShape(16.dp))
+                .background(Color.White, RoundedCornerShape(16.dp)),
+            shape = RoundedCornerShape(
+                bottomEnd = 16.dp,
+                bottomStart = 16.dp
+            ),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "BANK SAMPAH",
+                    fontSize = 24.sp,
+                    color = Color(0xFF4CAF50),
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Selamat Datang",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                )
+                Text(
+                    text = "di Aplikasi Bank Sampah",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                )
+            }
+        }
 
+        Spacer(modifier = Modifier.height(32.dp)) // Tambahkan sedikit ruang antara header dan CardScreen
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            CardScreen(navController = navController)
+        }
+
+
+        // Navigate to EmptyScreen when the button is clicked
+        Button(
+            onClick = { navController.navigate("emptyScreen") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text(text = "Go to Empty Screen")
+        }
+    }
+}
 @Composable
 fun CardScreen(navController: NavHostController) {
     Column(
