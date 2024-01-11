a8f84da660f21009c56303e6cc7a7ca384143d82
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-01-11 18:48:32 +0700

    Add string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.pambanksampah"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pambanksampah"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
